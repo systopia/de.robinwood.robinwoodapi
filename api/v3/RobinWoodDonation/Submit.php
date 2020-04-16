c2699ec604c3743a5881b0f16454583383519474
Jens Schuppe <schuppe@systopia.de>
2020-04-16 10:08:43 +0200

    Reverse payment instrument mapping

--- api/v3/RobinWoodDonation/Submit.php
+++ api/v3/RobinWoodDonation/Submit.php
@@ -248,24 +248,22 @@ function civicrm_api3_robin_wood_donation_Submit($params) {
 
     // Map payment methods to payment_instrument_id values.
     $payment_instruments = array(
-      'sepa' => 'sepa_direct_debit',
-      CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_STANDING_ORDER => 'dauerauftrag',
-      // TODO: Replace test payment methods with real ones.
-//      CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_CREDIT_CARD => 'commerce_novalnet_cc',
-//      CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_PAYPAL => 'commerce_novalnet_paypal',
-//      CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_SOFORT => 'commerce_novalnet_instantbank',
-      // Test payment methods.
-      CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_CREDIT_CARD => 'kreditkarte_testmodus_',
-      CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_PAYPAL => 'paypal_testmodus_',
-      CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_SOFORT => 'sofort_berweisung_testmodus_',
+      'sepa_direct_debit' => 'sepa',
+      'dauerauftrag' => CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_STANDING_ORDER,
+      'commerce_novalnet_cc' => CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_CREDIT_CARD,
+      'commerce_novalnet_paypal' => CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_PAYPAL,
+      'commerce_novalnet_instantbank' => CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_SOFORT,
+      'kreditkarte_testmodus_' => CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_CREDIT_CARD,
+      'paypal_testmodus_' => CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_PAYPAL,
+      'sofort_berweisung_testmodus_' => CRM_RobinWoodAPI_Submission::PAYMENT_INSTRUMENT_ID_NOVALNET_SOFORT,
     );
-    if (!in_array($params['payment_instrument'], $payment_instruments)) {
+    if (!array_key_exists($params['payment_instrument'], $payment_instruments)) {
       throw new Exception(E::ts('Invalid value for parameter %1', array(
         1 => 'payment_instrument',
       )));
     }
     else {
-      $params['payment_instrument_id'] = array_search($params['payment_instrument'], $payment_instruments);
+      $params['payment_instrument_id'] = $payment_instruments[$params['payment_instrument']];
     }
 
     // Resolve country ISO code.
